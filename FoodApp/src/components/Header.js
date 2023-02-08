e6494e0cd1c9c93bddec825e35dfada09eb63ef9
shailendra trivedi <shialendratrivedi009@gmail.com>
2023-02-08 23:24:40 +0530

    FoodApp

--- FoodApp/src/components/Header.js
+++ FoodApp/src/components/Header.js
@@ -5,8 +5,8 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
+
 // Icon
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import LogoutIcon from "@mui/icons-material/Logout";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 const Header = () => {
@@ -28,13 +28,26 @@ const Header = () => {
       setIsMenu(!isMenu);
     }
   };
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
   return (
     <>
       <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16  ">
         {/* Laptop && Tablet */}
         <div className="relative hidden md:flex w-full h-full items-center justify-between">
           <Link to={"/"} className="flex items-center gap-2">
-            <img src="./img/logo.png" className="w-8 object-cover" alt="logo" />
+            <img
+              src="./img/logo.png"
+              className="w-12 object-cover"
+              alt="logo"
+            />
             <p className="text-headingColor text-xl font-bold"> City</p>
           </Link>
           <div className="flex items-center gap-8 ">
@@ -58,8 +71,22 @@ const Header = () => {
               </li>
             </motion.ul>
             <div className="relative flex items-center justify-center p-4">
-              <ShoppingCartIcon className="text-textColor text-2xl  cursor-pointer" />
-              <p className="absolute text-white font-bold text-xs top-0 right-2 flex bg-blue-800 h-5 w-5 justify-center items-center rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="black"
+                viewBox="0 0 24 24"
+                stroke-width="0.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+
+              <p className="absolute text-white font-bold text-xs top-0 right-2 flex bg-blue-600 h-5 w-5 justify-center items-center rounded-full">
                 1
               </p>
             </div>
@@ -67,12 +94,17 @@ const Header = () => {
               <motion.img
                 whileTap={{ scale: 0.6 }}
                 src={user ? user.photoURL : "./img/avatar.png"}
-                className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+                className="w-12 min-w-[40px] h-12 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
                 alt="userprofile"
                 onClick={login}
               />
               {isMenu && (
-                <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.6 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.6 }}
+                  className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+                >
                   {user && user.email === "shailendratrivedi004@gmail.com" && (
                     <Link to={"/createItem"}>
                       <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-md text-base">
@@ -81,16 +113,99 @@ const Header = () => {
                     </Link>
                   )}
 
-                  <p className="px-4 py-2 flex items-center  gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-md text-base">
+                  <p
+                    className="px-4 py-2 flex items-center bg-slate-300 gap-3 cursor-pointer text-textColor rounded-md text-base"
+                    onClick={logout}
+                  >
                     <LogoutIcon /> Logout
                   </p>
-                </div>
+                </motion.div>
               )}
             </div>
           </div>
         </div>
         {/* Mobile */}
-        <div className=""></div>
+        <div className="flex items-center justify-between md:hidden w-full h-full ">
+          <div className="relative flex items-center justify-center">
+            <div className="relative flex items-center justify-center p-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="black"
+                viewBox="0 0 24 24"
+                stroke-width="0.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+
+              <p className="absolute text-white font-bold text-xs top-0 right-2 flex bg-blue-600 h-5 w-5 justify-center items-center rounded-full">
+                1
+              </p>
+            </div>
+          </div>
+
+          <Link to={"/"} className="flex items-center gap-2">
+            <img
+              src="./img/logo.png"
+              className="w-14 object-cover"
+              alt="logo"
+            />
+            <p className="text-headingColor text-xl font-bold"> City</p>
+          </Link>
+
+          <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={user ? user.photoURL : "./img/avatar.png"}
+              className="w-12 min-w-[40px] h-12 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              alt="userprofile"
+              onClick={login}
+            />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+              >
+                {user && user.email === "shailendratrivedi004@gmail.com" && (
+                  <Link to={"/createItem"}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-md text-base">
+                      <AddCircleOutlineIcon /> New Item
+                    </p>
+                  </Link>
+                )}
+
+                <ul className="flex flex-col ">
+                  <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                    Home
+                  </li>
+                  <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                    Menu
+                  </li>
+                  <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                    About Us
+                  </li>
+                  <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                    Service
+                  </li>
+                </ul>
+
+                <p
+                  className="px-4 py-2 flex items-center  gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-md text-base "
+                  onClick={logout}
+                >
+                  <LogoutIcon /> Logout
+                </p>
+              </motion.div>
+            )}
+          </div>
+        </div>
       </header>
     </>
   );
